e753bf25b450963fbe7283e6e2891ca36b4037ef
RandomCollection <99690701+RandomCollection@users.noreply.github.com>
2023-02-26 18:41:18 +0100

    Update database.py

--- database.py
+++ database.py
@@ -14,7 +14,7 @@ import sqlite3
 
 class Database:
 	def __init__(self):
-		self.con = sqlite3.connect(database=r".\data.db")  # TODO: r"data.db"
+		self.con = sqlite3.connect(database=r"data.db")
 		self.cursor = self.con.cursor()
 
 	def get_count_of_words_per_level(self) -> list:
